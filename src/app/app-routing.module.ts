070d6c78957617ac00fdd4573dbc005d7eb85fc9
hoanglinh1119 <hoanglinhjp2710@gmail.com>
2020-06-18 11:58:53 +0700

    add search and detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,11 +7,6 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./user/user.module').then(m => m.UserModule)
   }
-  // {
-  //   path: 'host',
-  //   loadChildren: () => import('./host/host.module').then(m => m.HostModule)
-  //
-  // }
 ];
 
 @NgModule({
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -5,6 +5,8 @@ import { FooterComponent } from './footer/footer.component';
 import { LayoutComponent } from './layout/layout.component';
 import {RouterModule, Routes} from '@angular/router';
 import {HomePageComponent} from './home-page/home-page.component';
+import { ResultSearchComponent } from './result-search/result-search.component';
+import { RoomDetailComponent } from './room-detail/room-detail.component';
 
 
 const routes: Routes = [
@@ -15,12 +17,20 @@ const routes: Routes = [
       {
         path: '',
         component: HomePageComponent
+      },
+      {
+        path: 'search-room',
+        component: ResultSearchComponent
+      },
+      {
+        path: 'room-detail',
+        component: RoomDetailComponent
       }
     ]
   }
 ];
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, LayoutComponent],
+  declarations: [HeaderComponent, FooterComponent, LayoutComponent, ResultSearchComponent, RoomDetailComponent],
     imports: [
         CommonModule,
         RouterModule.forChild(routes)
