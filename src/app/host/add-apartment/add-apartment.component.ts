1e6042718367cc12465fa75f80cd6eaf12de1620
phanvietdung2101 <59188700+phanvietdung2101@users.noreply.github.com>
2020-06-18 21:51:24 +0700

    add async function in add-apartment

--- src/app/host/add-apartment/add-apartment.component.ts
+++ src/app/host/add-apartment/add-apartment.component.ts
@@ -1,3 +1,5 @@
+import { Apartment } from './../../model/apartment';
+import { async } from '@angular/core/testing';
 import { Category } from './../../model/category';
 import { Picture } from './../../model/picture';
 import { Component, OnInit } from '@angular/core';
@@ -16,28 +18,13 @@ import { IDropdownSettings } from 'ng-multiselect-dropdown';
   styleUrls: ['./add-apartment.component.css'],
 })
 export class AddApartmentComponent implements OnInit {
-  settings: IDropdownSettings = {
-    singleSelection: false,
-    idField: 'id',
-    textField: 'name',
-    enableCheckAll: true,
-    selectAllText: 'Select all',
-    unSelectAllText: 'Unselect all',
-    allowSearchFilter: true,
-    limitSelection: -1,
-    clearSearchFilter: true,
-    maxHeight: 197,
-    itemsShowLimit: 10,
-    searchPlaceholderText: 'Search',
-    noDataAvailablePlaceholderText: 'No value',
-    closeDropDownOnSelection: false,
-    showSelectedItemsAtTop: false,
-    defaultOpen: false  
-  }
+  settings: IDropdownSettings = {}
+  
   categories = [] ;
   roomTypes = [];
-
+  apartment: Apartment;
   // For Upload
+  pictures: Picture[] = [];
   listUrl: string[] = [];
   apartmentForm: FormGroup;
 
@@ -62,6 +49,25 @@ export class AddApartmentComponent implements OnInit {
       { id: 2, name: 'phong 2'},
       { id: 3, name: 'phong 3'},
     ]
+
+    this.settings = {
+      singleSelection: false,
+      idField: 'id',
+      textField: 'name',
+      enableCheckAll: true,
+      selectAllText: 'Select all',
+      unSelectAllText: 'Unselect all',
+      allowSearchFilter: true,
+      limitSelection: -1,
+      clearSearchFilter: true,
+      maxHeight: 197,
+      itemsShowLimit: 10,
+      searchPlaceholderText: 'Search',
+      noDataAvailablePlaceholderText: 'No value',
+      closeDropDownOnSelection: false,
+      showSelectedItemsAtTop: false,
+      defaultOpen: false  
+    }
     // setting and support i18n
   
 
@@ -88,21 +94,36 @@ export class AddApartmentComponent implements OnInit {
   pictureList: Picture[] = [];
 
   onSubmit() {
-    console.log(this.apartmentForm.value)
+    // if(!this.apartmentForm.invalid){
+    //   return;
+    // }
+    this.files.forEach(async (file) => {
+       const promise = await this.startUpload(file)
+       .then(res => console.log(res))
+       .catch(err => console.log(err))
+       
+    })
+    this.apartment = this.apartmentForm.value;
+    this.apartment.pictures = this.pictures;
+    console.log(this.apartment)
+    // make apartment object empty
+    this.apartment = {};
   }
 
   onSelect(event) {
     console.log(event);
     this.files.push(...event.addedFiles);
-    this.startUpload(event.addedFiles[0]);
+    console.log(this.files)
   }
 
   onRemove(event) {
     console.log(event);
     this.files.splice(this.files.indexOf(event), 1);
+    console.log(this.files)
+
   }
 
-  startUpload(file: File) {
+  async startUpload(file: File) {
     // The storage path
     const path = `hotel/${Date.now()}_${file.name}`;
 
@@ -110,26 +131,18 @@ export class AddApartmentComponent implements OnInit {
     const ref = this.storage.ref(path);
 
     // The main task
-    this.storage
+    return this.storage
       .upload(path, file)
       .snapshotChanges()
       .pipe(
         finalize(() => {
           ref.getDownloadURL().subscribe((url) => {
-            this.listUrl.push(url);
-            console.log(this.listUrl);
+            this.pictures.push({
+              imageUrl: url
+            });
+            console.log(this.pictures);
           });
         })
-      )
-      .subscribe(
-        (res) => {
-          console.log(res);
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
-
-    console.log(this.listUrl);
+      ).subscribe();
   }
 }
