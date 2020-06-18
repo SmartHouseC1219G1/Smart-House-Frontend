bd422963a00e1b4fa2bf61baad6e340fe1206483
hoanglinh1119 <hoanglinhjp2710@gmail.com>
2020-06-18 21:47:51 +0700

    add home-page.component.ts

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -8,8 +8,8 @@ import {map} from 'rxjs/operators';
   providedIn: 'root'
 })
 export class UserService {
-  urlList = 'http://localhost:8080/listApartment';
-  urlListById = 'http://localhost:8080/listApartment/{id}';
+  urlList = 'http://localhost:8080/api/listApartment';
+  urlListById = 'http://localhost:8080/api/listApartment/{id}';
   constructor(private httpClient: HttpClient) { }
   listApartment(count = 10): Observable<Apartment[]> {
     return this.httpClient.get<Apartment[]>(this.urlList).pipe(map(response => response.filter((post, i) => i < count)));
