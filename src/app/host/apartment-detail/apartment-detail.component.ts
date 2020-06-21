4d12e5308a9019d5a4174857e25a314c90087b65
phanvietdung2101 <59188700+phanvietdung2101@users.noreply.github.com>
2020-06-21 16:56:50 +0700

    host update picture list -> done

--- src/app/host/apartment-detail/apartment-detail.component.ts
+++ src/app/host/apartment-detail/apartment-detail.component.ts
@@ -1,3 +1,7 @@
+import { async } from '@angular/core/testing';
+import { finalize } from 'rxjs/operators';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { Picture } from './../../model/picture';
 import { Res } from './../../model/res';
 import { ApartmentService } from './../../service/apartment.service';
 import { ActivatedRoute } from '@angular/router';
@@ -7,23 +11,39 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-apartment-detail',
   templateUrl: './apartment-detail.component.html',
-  styleUrls: ['./apartment-detail.component.css']
+  styleUrls: ['./apartment-detail.component.css'],
 })
 export class ApartmentDetailComponent implements OnInit {
-
+  files: File[] = [];
+  pictures: Picture[] = [];
   apartment: Apartment;
   constructor(
+    private storage: AngularFireStorage,
     private apartmentService: ApartmentService,
     private route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
-    
+    // get apartment
     const id = +this.route.snapshot.paramMap.get('id');
     this.apartmentService.getDetailApartmentById(id).subscribe(
       (data: Res) => {
         this.apartment = data.data;
-        console.log(this.apartment)
+        //get imageurl to files
+        let fetches = [];
+        for (let i = 0; i < this.apartment.pictures.length; i++) {
+          const url = this.apartment.pictures[i].imageUrl;
+          fetches.push(this.fetchImage(url));
+          console.log(fetches);
+        }
+
+        Promise.all(fetches)
+          .then((res) => {
+            console.log(res);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
       },
       (error) => {
         console.log(error);
@@ -32,4 +52,76 @@ export class ApartmentDetailComponent implements OnInit {
     );
   }
 
+  onSelect(event: { addedFiles: any }) {
+    console.log(event);
+    this.files.push(...event.addedFiles);
+    console.log(this.files);
+  }
+
+  onRemove(event: File) {
+    console.log(event);
+    this.files.splice(this.files.indexOf(event), 1);
+    console.log(this.files);
+  }
+
+  fetchImage(url: string) {
+    var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    return fetch(proxyUrl + url)
+      .then((res) => res.blob()) // Gets the response and returns it as a blob
+      .then((blob) => {
+        // convert blob to file object
+        const file = new File([blob], `image${Date.now()}.jpg`, {
+          type: blob.type,
+        });
+        //push file to files
+        this.files.push(file);
+        console.log(this.files);
+      });
+  }
+
+  startUpload(file: File) {
+    // The storage path
+    const path = `hotel/${Date.now()}_${file.name}`;
+
+    // Reference to storage bucket
+    const ref = this.storage.ref(path);
+
+    // The main task
+    return this.storage.upload(path, file).snapshotChanges().toPromise();
+  }
+
+  async onSave() {
+    console.log('saving');
+
+    const uploadArray = [];
+    this.files.forEach((file) => {
+      uploadArray.push(this.startUpload(file));
+    });
+    console.log('before upload');
+
+    Promise.all(uploadArray)
+      .then(async (result) => {
+        console.log(result);
+        for (let i = 0; i < result.length; i++) {
+          const element = result[i];
+          const imageUrl = await element.ref.getDownloadURL();
+          this.pictures.push({
+            imageUrl: imageUrl,
+          });
+          console.log('pass' + i);
+        }
+        console.log(this.pictures);
+      })
+      .then(() => {
+        this.apartmentService
+          .updateApartmentPictures(this.apartment.id, this.pictures)
+          .subscribe(
+            (data) => {
+              console.log(data);
+            },
+            (err) => console.log(err)
+          );
+      })
+      .catch((err) => console.log(err));
+  }
 }
