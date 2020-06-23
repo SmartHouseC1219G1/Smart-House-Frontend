22f01052c7d2f2774cc9f7f9435f33001913ded3
phanvietdung2101 <59188700+phanvietdung2101@users.noreply.github.com>
2020-06-23 15:36:51 +0700

    add sweetalert2

--- src/app/login/sign-in/sign-in.component.ts
+++ src/app/login/sign-in/sign-in.component.ts
@@ -1,13 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import Swal from 'sweetalert2';
 
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  onOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }
+})
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.css']
 })
-export class SignInComponent implements OnInit {
 
+
+export class SignInComponent implements OnInit {
   loginForm: FormGroup;
   constructor() { }
 
@@ -18,4 +31,16 @@ export class SignInComponent implements OnInit {
       password: new FormControl('', [Validators.required, Validators.minLength(6)])
     });
   }
+
+  onSubmit(){
+    console.log(this.loginForm);
+    Toast.fire({
+      icon: 'success',
+      title: 'Signed in successfully'
+    })
+  }
+
+ 
+  
+  
 }
